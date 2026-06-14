f55f9f4dbc5c707f4cbc0f4d7dcdc957da8102d6
Hazik Anwar <3630449+HazAnwar@users.noreply.github.com>
2026-06-14 18:59:23 +0100

    update email address

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export default function Terms() {
   return (
-    <div style={{ padding: '4rem 0' }}>
-      <div className='glass' style={{ padding: '3rem', borderRadius: '1rem' }}>
-        <h1 style={{ marginBottom: '2rem' }}>Terms of Service</h1>
-        <p style={{ color: 'var(--text-secondary)' }}>
-          By using any applications provided by HazTech Services Limited, you agree to comply with our terms of service. Our applications are provided "as is" without warranties of any kind.
-          <br /><br />
-          HazTech Services Limited reserves the right to modify or terminate any services or applications at any time without prior notice.
-          <br /><br />
-          If you encounter any issues or need further assistance, please reach out to our support team at HazTechServices@gmail.com.
+    <div style={{ padding: "4rem 0" }}>
+      <div className="glass" style={{ padding: "3rem", borderRadius: "1rem" }}>
+        <h1 style={{ marginBottom: "2rem" }}>Terms of Service</h1>
+        <p style={{ color: "var(--text-secondary)" }}>
+          By using any applications provided by HazTech Services Limited, you
+          agree to comply with our terms of service. Our applications are
+          provided "as is" without warranties of any kind.
+          <br />
+          <br />
+          HazTech Services Limited reserves the right to modify or terminate any
+          services or applications at any time without prior notice.
+          <br />
+          <br />
+          If you encounter any issues or need further assistance, please reach
+          out to our support team at info@haztech.services.
         </p>
       </div>
     </div>
